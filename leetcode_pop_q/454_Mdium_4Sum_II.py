aca6397f84b7cfc416d7f87d683ab5979ed64b49
Audrey Chen <zhijiao_chen@hotmail.com>
2020-05-10 00:05:13 -0400

    0509

---
+++ leetcode_pop_q/454_Mdium_4Sum_II.py
@@ -0,0 +1,17 @@
+"""
+Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
+"""
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        absum = {}
+        ans = 0
+        for a in A:
+            for b in B:
+                absum[a+b] = absum.get(a+b, 0) + 1
+        for c in C:
+            for d in D:
+                ans += absum.get(-(c+d), 0)
+        return ans

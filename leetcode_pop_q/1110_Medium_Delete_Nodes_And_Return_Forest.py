59e7eb05048e5411c19d0e093d5f411a2e5d213b
zchen-stash <zchen@stashinvest.com>
2021-01-01 21:23:17 -0500

    20210101

---
+++ leetcode_pop_q/1110_Medium_Delete_Nodes_And_Return_Forest.py
@@ -0,0 +1,36 @@
+"""
+🌸 Medium 1110. Delete Nodes And Return Forest
+
+Given the root of a binary tree, each node in the tree has a distinct value.
+
+After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+
+Return the roots of the trees in the remaining forest.  You may return the result in any order.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
+        ans = []
+        to_delete_set = set(to_delete)
+        def checkDel(node, parent_del):
+            if not node:
+                return None
+            delete_node = node.val in to_delete_set
+            if not delete_node and parent_del:
+                ans.append(node)
+            node.left = checkDel(node.left, delete_node)
+            node.right = checkDel(node.right, delete_node)
+            return None if delete_node else node
+        
+        checkDel(root, True)
+        return ans

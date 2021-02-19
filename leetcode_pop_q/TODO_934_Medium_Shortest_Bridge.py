5089bee29b8778919c1b57dd9521ba27ec8474b7
Audrey Chen <zhijiao_chen@hotmail.com>
2021-02-18 23:48:13 -0500

    todo

---
+++ leetcode_pop_q/TODO_934_Medium_Shortest_Bridge.py
@@ -0,0 +1,66 @@
+"""
+934. Shortest Bridge
+Medium
+
+1183
+
+82
+
+Add to List
+
+Share
+In a given 2D binary array A, there are two islands.  (An island is a 4-directionally connected group of 1s not connected to any other 1s.)
+
+Now, we may change 0s to 1s so as to connect the two islands together to form 1 island.
+
+Return the smallest number of 0s that must be flipped.  (It is guaranteed that the answer is at least 1.)
+
+ 
+
+Example 1:
+
+Input: A = [[0,1],[1,0]]
+Output: 1
+Example 2:
+
+Input: A = [[0,1,0],[0,0,0],[0,0,1]]
+Output: 2
+Example 3:
+
+Input: A = [[1,1,1,1,1],[1,0,0,0,1],[1,0,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+Output: 1
+ 
+
+Constraints:
+
+2 <= A.length == A[0].length <= 100
+A[i][j] == 0 or A[i][j] == 1
+
+"""
+class Solution:
+    def shortestBridge(self, A):
+        def dfs(i, j):
+            A[i][j] = -1
+            bfs.append((i, j))
+            for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                if 0 <= x < n and 0 <= y < n and A[x][y] == 1:
+                    dfs(x, y)
+        def first():
+            for i in range(n):
+                for j in range(n):
+                    if A[i][j]:
+                        return i, j
+        n, step, bfs = len(A), 0, []
+        dfs(*first())
+        while bfs:
+            new = []
+            for i, j in bfs:
+                for x, y in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
+                    if 0 <= x < n and 0 <= y < n:
+                        if A[x][y] == 1:
+                            return step
+                        elif not A[x][y]:
+                            A[x][y] = -1
+                            new.append((x, y))
+            step += 1
+            bfs = new

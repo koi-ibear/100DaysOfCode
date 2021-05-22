d9130483c521edf948b0fb51489e9ece500e7016
Audrey Chen <zhijiao_chen@hotmail.com>
2021-05-21 21:10:05 -0400

    tree

---
+++ leetcode_pop_q/98_Medium_Validate_Binary_Search_Tree.py
@@ -0,0 +1,28 @@
+"""
+Medium 98. Validate Binary Search Tree
+
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+Example 1:
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+"""
+def isValidBST(self, root: TreeNode, floor=-float('inf'), ceil=float('inf')) -> bool:
+    if not root: return True
+    if root.val >= ceil or root.val <= floor:
+        return False
+    return self.isValidBST(root.left, floor, min(ceil, root.val)) and self.isValidBST(root.right, max(floor, root.val), ceil)
+    
